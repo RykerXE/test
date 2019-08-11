423251c2892609abe791f0013688a258592a3b08
unknown <rajnish.kumar@byjus.com>
2019-08-11 14:05:12 +0530

    Added new logo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src='https://upload.wikimedia.org/wikipedia/en/1/1c/IIT_Kharagpur_Logo.svg'className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
